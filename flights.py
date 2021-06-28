140a1bdeb196ec93d2724d10a6fe65cd21897847
Will Smith <william.e.smithiv@gmail.com>
2021-06-28 11:22:51 -0600

    more data cleaning and updated flights map

--- flights.py
+++ flights.py
@@ -4,26 +4,29 @@ import pydeck as pdk
 from streamlit.script_runner import ScriptControlException
 
 # Data import
-routes = pd.read_csv('./routes.csv')
+coords = pd.read_csv('./full_routes_data.csv')
+st.dataframe(coords)
 
-st.dataframe(routes)
+us_data = coords.loc[(coords.src_Country == "United States") & (coords.dest_Country == "United States") & (coords.Airline == "AA")]
+
+st.dataframe(us_data)
+
+st.write("Distinct Airlines")
+st.write(coords.Airline.unique())
 
 
-st.dataframe(routes.isna())
+arc_layer = pdk.Layer(
+    "ArcLayer",
+    data=us_data,
+    get_source_position=["src_Longitude","src_Latitude"],
+    get_target_position=["dest_Longitude","dest_Latitude"],
+    get_tilt=15,
+    get_source_color=[0,255,0,40],
+    get_target_color=[240,100,0,40]
+)
 
+deck = pdk.Deck(
+    layers=[arc_layer]
+)
 
-# arc_layer = pdk.Layer(
-#     "ArcLayer",
-#     data=routes[['src_Longitude','src_Latitude','dest_Longitude','dest_Latitude']],
-#     get_source_position=["src_Longitude","src_Latitude"],
-#     get_target_position=["dest_Longitude","dest_Latitude"],
-#     get_tilt=15,
-#     get_source_color=[0,255,0,40],
-#     get_target_color=[240,100,0,40]
-# )
-
-# deck = pdk.Deck(
-#     layers=[arc_layer]
-# )
-
-# st.pydeck_chart(deck)
+st.pydeck_chart(deck)
